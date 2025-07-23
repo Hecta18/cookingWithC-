669b20c86e6c36fb8881de45209466bcbfad0eaf
Hecta18 <62799494+Hecta18@users.noreply.github.com>
2025-07-22 18:00:51 -0600

    Learning about structuring a C++ project

---
+++ src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp
+
+// directive to indicate that i/o possible
+#include <iostream>
+// the using directive allows us to use 
+// names from the std namespace without the std:: prefix
+using namespace std;
+// my first C++ program
+int main() {
+    // cout only works with iostream
+    cout << "Hello World!" << "\n I'm back baby!" << endl;
+    // << means delivering information to the output stream = insert
+    // endl means end of line
+    return 0;
+}
